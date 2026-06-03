849f2cf12251805617036700525b72e76f8b2e95
faustocargnelutti16-a11y <faustocargnelutti16@gmail.com>
2026-06-03 20:58:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -76,4 +76,7 @@ app.post('/webhook', (req, res) => {
   }
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
+  console.log('Server running');
+});
+
