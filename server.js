598a9877f3c94701af086c989ec8a3d05ccb4ee4
faustocargnelutti16-a11y <faustocargnelutti16@gmail.com>
2026-06-04 20:42:26 -0300

    Update server.js

--- server.js
+++ server.js
@@ -128,7 +128,4 @@ app.post('/webhook', (req, res) => {
   }
 });
 
-app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
-  console.log('Server running');
-  crearOrden();
-});
+app.listen(process.env.PORT ||
